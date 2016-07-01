853f62f7bf5838a04b45d5753ac06a4dd4291b6d
Miguel Ramos <ramos.miguel09@gmail.com>
2016-07-01 20:04:32 -0300

    este es el primer commit que hago

---
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -0,0 +1,10 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	var a;
+	a=prompt("ingrese un numero","0");
+	alert(a);
+	
+}
+
