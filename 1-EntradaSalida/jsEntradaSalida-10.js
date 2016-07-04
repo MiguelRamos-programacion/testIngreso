4474e99653e98058199302ff1bd49543218c13f7
MiguelRamos-programacion <ramos.miguel09@gmail.com>
2016-07-03 22:26:14 -0300

    Update jsEntradaSalida-10.js

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,13 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
-	
+var importe;
+		importe=document.getElementById('importe').value;
+		importe=parseInt(importe);
+	var Descuento;
+		Descuento=(importe*0.25);
+		Descuento=parseInt(Descuento);
+		importe=importe-Descuento;		
+
+		alert("el importe con un descuento del 25% es: "+importe);	
 }
