259842c2a734161b82401b3519f9d99c90873e81
MiguelRamos-programacion <ramos.miguel09@gmail.com>
2016-07-03 22:20:14 -0300

    Update jsEntradaSalida-5.js

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,12 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function Mostar()
 {	
-	
+var nombre;
+	nombre=document.getElementById('elNombre').value;
+
+	var edad;
+	edad=document.getElementById('laEdad').value;
+
+	alert("usted se llama "+nombre+" y su edad es de "+edad+"años");	
 }
 
