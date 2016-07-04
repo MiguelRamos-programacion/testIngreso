83da4401cdcade002b22263393d4a9a47d8ee436
MiguelRamos-programacion <ramos.miguel09@gmail.com>
2016-07-03 22:25:26 -0300

    Update jsEntradaSalida-9.js

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,10 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
-
+var sueldo;
+		sueldo=document.getElementById('sueldo').value;
+		sueldo=parseInt(sueldo);
+		sueldo=sueldo*1.10;
+		alert("monto con un incremento del 10%: "+sueldo);
 	
 }
